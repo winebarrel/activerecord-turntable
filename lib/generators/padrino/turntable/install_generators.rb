261a078a2070504e6012fc8ed5b69beb0b782265
Shigeki Morimoto <Shigeki@Morimo.to>
2014-01-25 18:07:15 +0900

    add generator for turntable with padrino.

---
+++ lib/generators/padrino/turntable/install_generators.rb
@@ -0,0 +1,29 @@
+require 'padrino-gen'
+
+module Padrino
+  module Generators
+    class Turntable < Thor::Group
+      Padrino::Generators.add_generator(:turntable, self)
+
+      def self.source_root
+        File.expand_path("../../../templates", __FILE__)
+      end
+
+      desc "Creates turntable configuration file (config/turntable.yml)"
+
+      include Thor::Actions
+      include Padrino::Generators::Actions
+      include Padrino::Generators::Components::Actions
+
+      desc "Creates turntable configuration file (config/turntable.yml)"
+
+      def self.require_arguments?
+        false
+      end
+
+      def create_turntable
+        copy_file "turntable.yml", "config/turntable.yml"
+      end
+    end
+  end
+end
